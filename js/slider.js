e6418f2b038475c7552da2132dd9ca1659d1e25e
Dmitry Kim <kim.dima.93@gmail.com>
2021-03-21 23:02:09 +0300

    week 3 corrections

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,48 @@
-const prevBtn = document.querySelector('.products-slider__arrow--type--prev')
-const nextBtn = document.querySelector('.products-slider__arrow--type--next')
+(function (){
+  const items = document.querySelectorAll(".product");
+  const container = document.querySelector(".slider__list");
+  const nextBtn = document.querySelectorAll('.slider__arrow--type--next');
+  const prevBtn = document.querySelectorAll('.slider__arrow--type--prev');
 
+  let step = setCurrentWidth();
+  const maxRight = step * items.length;
+  let currentRight = 0;
+
+  container.style.right = currentRight;
+
+  window.addEventListener("resize", () => {
+    step = setCurrentWidth();
+  })
+
+  nextBtn.addEventListener("click", (e) => {
+    changeSlide(e, "right");
+  });
+
+  prevBtn.addEventListener("click", (e) => {
+    changeSlide(e, "left");
+  });
+
+  function changeSlide(e, direction){
+    e.preventDefault();
+    if (direction == "right"){
+      if (currentRight < maxRight){
+        currentRight += step;
+        container.style.right = `${currentRight}px`
+      }
+      if (currentRight == maxRight){
+        currentRight = 0;
+        container.style.right = `$`
+      }
+    } else {
+
+    }
+  }
+
+  function setCurrentWidth(){
+    let itemWidth = document.querySelector(".products-wrapper"). 
+    items.forEach(item => {
+      item.style.width = `${itemWidth}px`
+    })
+    return itemWidth
+  }
+})();
