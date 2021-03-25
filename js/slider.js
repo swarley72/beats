1fcfcee878812a0fcc8100812ce922a54f67185e
Dmitry Kim <kim.dima.93@gmail.com>
2021-03-25 23:39:53 +0300

    bxslidr comleted (almost)

--- js/slider.js
+++ js/slider.js
@@ -1,52 +1,33 @@
-// (function (){
-//   const items = document.querySelectorAll(".product");
-//   const container = document.querySelector(".slider__list");
-//   const nextBtn = document.querySelectorAll('.slider__arrow--type--next');
-//   const prevBtn = document.querySelectorAll('.slider__arrow--type--prev');
+function getWidth (){
+  const sliderContainerWidth = $(".slider__container").innerWidth();
+  return sliderContainerWidth;
+};
 
-//   let step = setCurrentWidth();
-//   const maxRight = step * items.length;
-//   let currentRight = 0;
 
-//   container.style.right = currentRight;
 
-//   window.addEventListener("resize", () => {
-//     step = setCurrentWidth();
-//   })
+let sliderWrapper = document.querySelector(".slider__list-wrapper").style.width = `${getWidth()}px`
 
-//   nextBtn.addEventListener("click", (e) => {
-//     changeSlide(e, "right");
-//   });
+window.addEventListener("resize", e => {  
+  sliderWrapper.style.width = `${getWidth()}px`
+})
 
-//   prevBtn.addEventListener("click", (e) => {
-//     changeSlide(e, "left");
-//   });
 
-//   function changeSlide(e, direction){
-//     e.preventDefault();
-//     if (direction == "right"){
-//       if (currentRight < maxRight){
-//         currentRight += step;
-//         container.style.right = `${currentRight}px`
-//       }
-//       if (currentRight == maxRight){
-//         currentRight = 0;
-//         container.style.right = `$`
-//       }
-//     } else {
+const slider = $('.slider__list').bxSlider({
+  pager: false,
+  controls: false
+});
 
-//     }
-//   }
+$(".slider__arrow--type--prev").click(e => {
+  e.preventDefault();
+  slider.goToPrevSlide();
+});
 
-//   function setCurrentWidth(){
-//     let itemWidth = document.querySelector(".products-wrapper"). 
-//     items.forEach(item => {
-//       item.style.width = `${itemWidth}px`
-//     })
-//     return itemWidth
-//   }
-// })();
+$(".slider__arrow--type--next").click(e => {
+  e.preventDefault();
+  slider.goToNextSlide();
+});
 
-// $(document).ready(function(){
-//   $('.slider__list').slick();
-// });
+
+
+
+
